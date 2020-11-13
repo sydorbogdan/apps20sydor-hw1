75443c44067f80de3689b729f183eb0846348ac0
Bohdan Sydor <bohdansydor@pop-os.localdomain>
2020-11-13 20:13:07 +0200

    updated tests

--- src/main/java/dynArr/DynDoubleArray.java
+++ src/main/java/dynArr/DynDoubleArray.java
@@ -7,67 +7,64 @@ public class DynDoubleArray {
     private int size; //last index
     private double[] data;
 
-    public DynDoubleArray(){
+    public DynDoubleArray() {
         data = new double[]{0};
         capacity = 1;
         size = -1;
     }
 
-    public DynDoubleArray(double[] inp_data){
+    public DynDoubleArray(double[] inp_data) {
         data = inp_data;
         capacity = inp_data.length;
-        size = inp_data.length-1;
+        size = inp_data.length - 1;
     }
 
-    public void add(double inp_elem){
-        if (capacity == size+1){
-            data = Arrays.copyOf(data, 2*capacity);
+    public void add(double inp_elem) {
+        if (capacity == size + 1) {
+            data = Arrays.copyOf(data, 2 * capacity);
             capacity *= 2;
         }
-        data[size+1] = inp_elem;
+        data[size + 1] = inp_elem;
         size += 1;
     }
 
-    public int len(){
+    public int len() {
         return size + 1;
     }
 
-    public double get(int ind)
-    {
+    public double get(int ind) {
         if (-1 < ind && ind < size + 1) {
             return data[ind];
-        } else{
+        } else {
             throw new IndexOutOfBoundsException();
         }
 
     }
 
 
-    public double min()
-    {
-        double curr_min = data[0];
-        for (int i=0; i < size+1; i++){
-            if (curr_min > data[i]) {
-                curr_min = data[i];
+    public double min() {
+        double currMin = data[0];
+        for (int i = 0; i < size + 1; i++) {
+            if (currMin > data[i]) {
+                currMin = data[i];
             }
         }
-        return curr_min;
+        return currMin;
     }
 
-    public double max()
-    {
-        double curr_max = data[0];
-        for (int i=0; i < size+1; i++){
-            if (curr_max < data[i]) {
-                curr_max = data[i];
+    public double max() {
+        double currMax = data[0];
+        for (int i = 0; i < size + 1; i++) {
+            if (currMax < data[i]) {
+                currMax = data[i];
             }
         }
-        return curr_max;
+        return currMax;
     }
 
     public double[] getData() {
-        double[] slice = new double[size+1];
-        for (int i=0; i < size+1; i++){
+        double[] slice = new double[size + 1];
+        for (int i = 0; i < size + 1; i++) {
             slice[i] = data[i];
         }
         return slice;
--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -6,7 +6,10 @@ public class TempSummaryStatistics {
     private double minTemp;
     private double maxTemp;
 
-    public TempSummaryStatistics(double average, double deviation, double min, double max) {
+    public TempSummaryStatistics(double average,
+                                 double deviation,
+                                 double min,
+                                 double max) {
         avgTemp = average;
         devTemp = deviation;
         minTemp = min;
